6248f626c7d5f5cb13f2e5a032947c207eafc560
Sasha Kirillov <sasha.anaximander@gmail.com>
2019-09-08 17:32:13 +0300

    added responsiveness to LandingPage

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -12,19 +12,27 @@ class LandingPage extends Component {
                 <div className="cover">
                     <div></div>
                     <div></div>
-                    <div className="introduction">
-                        <p className="intro-heading">House Explorer</p>
+                    <div className="intro-wrapper">
+                        <p className="intro-heading">House<br/>Explorer</p>
                         <p className="intro-text" >
                             Lorem ipsum odor amet, consectetuer adipiscing elit. 
                             Scelerisque mus neque class dolor. Adipiscing placerat tempor.
                         </p>
+                        <button className="intro-btn">get started</button>
                     </div>
-                    <img id="cover-image" alt="houses with a map pin over them" src={cover} />
+                    <div className="image-wrapper">
+                        <img className="cover-image" alt="houses with a map pin over them" src={cover} />
+                        <p className="image-credit">&copy; &nbsp;
+                            <a href="https://icons8.com/ouch/illustration/abstract-location-access" 
+                               rel="noopener noreferrer" target="_blank">Icons 8</a> 
+                        </p>
+                    </div>
                 </div>
                 <footer>
                     created by <i className="fab fa-github"></i>
                     &nbsp;  
-                    <a href="http://github.com/sasha-kir">sasha-kir</a>
+                    <a href="http://github.com/sasha-kir" 
+                       rel="noopener noreferrer" target="_blank">sasha-kir</a>
                 </footer>
             </div>
         );
