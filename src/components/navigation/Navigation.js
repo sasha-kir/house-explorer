0334c1f38ab639aa18ad0238144c637abba4cf1a
Sasha Kirillov <sasha.anaximander@gmail.com>
2019-11-04 22:27:33 +0300

    completed hamburger menu

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+
 import './Navigation.sass';
 import logo from '../../images/logo.svg';
 
@@ -10,8 +11,11 @@ class Navigation extends Component {
         this.state = {
             loading: true,
             loggedIn: false,
-            userPic: ""
+            userPic: "",
+            isMobile: window.innerWidth <= 950,
+            isHamburgerOpen: false
         }
+        this.sideMenu = React.createRef();
     }
 
     async componentDidMount() {
@@ -32,6 +36,11 @@ class Navigation extends Component {
             console.log('server error: could not check token');
             this.setState({ loading: false });
         }
+        window.addEventListener('resize', this.updateHamburger);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateHamburger);
     }
 
     handleLogoClick = () => {
@@ -42,66 +51,136 @@ class Navigation extends Component {
         this.props.history.push("/profile");
     }
 
+    contentSwitcher = () => {
+        let leftLinks = this.leftContentSwitcher();
+        let rightLinks = this.rightContentSwitcher();
+        if (!this.state.isMobile) {
+            let rightNav = (
+                <div className="right-nav">
+                    {rightLinks}
+                    <div className="arrow"></div>
+                </div>
+            );
+            return [leftLinks, rightNav];
+        } else {
+            let hamburgerButton = (
+                <button className="hamburger-button" onClick={this.toggleHamburger}>
+                    {this.state.isHamburgerOpen
+                        ? <i className="fas fa-times"></i>
+                        : <i className="fas fa-bars"></i>
+                    }
+                </button>
+            );
+            let hamburgerMenu = (
+                <div className="hamburger-menu">
+                    <ul ref={this.sideMenu} className={`side-menu ${this.state.isHamburgerOpen ? "visible" : ""}`}>
+                        {leftLinks.map(link => <li>{link}</li>)}
+                        <hr />
+                        {!this.state.loggedIn
+                            ?   [
+                                    <li>
+                                        {rightLinks[1]}
+                                        &nbsp; &nbsp; &nbsp; 
+                                        {rightLinks[0]}
+                                    </li>,
+                                ]
+                            :  rightLinks.map(link => <li>{link}</li>)
+                        }
+                    </ul>
+                </div>
+            );
+            return [hamburgerButton, hamburgerMenu];
+        }
+    }
+
     leftContentSwitcher = () => {
+        let navType = this.state.isMobile ? "side" : "left";
+        let leftLinks = [
+            <NavLink className={navType + "-nav-item"} to="/cities" id="cities-link">
+                cities
+            </NavLink>,
+            <NavLink className={navType + "-nav-item"} to="/about">
+                about
+            </NavLink>
+        ];
         if (this.state.loggedIn) {
-            return (
-                <NavLink to="/explore" id="explore-link">
+            leftLinks.unshift(
+                <NavLink className={navType + "-nav-item"} to="/explore" id="explore-link">
                     explore
                 </NavLink>
-            )
-        }
+            );
+        };
+        return leftLinks;
     }
 
     rightContentSwitcher = () => {
         if (this.state.loggedIn) {
-            return (
-                <div className="right-nav">
-                    <NavLink to="/history">
-                        history
-                    </NavLink>
+            let historyLink =  (
+                <NavLink className={this.state.isMobile ? "side-nav-item" : ""} to="/history">
+                    history
+                </NavLink>
+            );
+            let profileLink = <NavLink className="side-nav-item" to="/profile">profile</NavLink>;
+            let logoutLink = (
+                <NavLink to="/#" className={`${this.state.isMobile ? "side-nav-item" : ""} login-link`}
+                        onClick={this.props.handleLogOut}
+                >
+                    log out
+                </NavLink>)
+            ;
+            if (!this.state.isMobile) {
+                return [
+                    historyLink,
                     <img className="user-pic" alt="userpic" src={this.state.userPic} 
-                            onClick={this.handleUserpicClick}
-                    />
-                    <NavLink to="/#" className="login-link"
-                             onClick={this.props.handleLogOut}>
-                        log out
-                    </NavLink>
-                    <div className="arrow"></div>
-                </div>
-            );
+                             onClick={this.handleUserpicClick}
+                    />,
+                    logoutLink
+                ];
+            } else {
+                return [historyLink, profileLink, logoutLink];
+            }
         } else {
-            return (
-                <div className="right-nav">
-                    <NavLink to="/register" className="signup-btn">
-                        sign up
-                    </NavLink>
-                    <NavLink to="/login" className="login-link">
-                        log in
-                    </NavLink>
-                    <div className="arrow"></div>
-                </div>
-            );
+            return [
+                <NavLink to="/register" className={`${this.state.isMobile ? "side-nav-item" : ""} signup-btn`}>
+                    sign up
+                </NavLink>,
+                <NavLink to="/login" className={`${this.state.isMobile ? "side-nav-item" : ""} login-link`}>
+                    log in
+                </NavLink>
+            ];
+        }
+    }
+
+    toggleHamburger = () => {
+        if (!this.state.isHamburgerOpen) {
+            this.setState({ isHamburgerOpen: true });
+            this.sideMenu.current.style.right = (window.innerWidth - 300) * (-1) + "px";
+        } else {
+            this.setState({ isHamburgerOpen: false });
+        }
+    }
+
+    updateHamburger = () => {
+        let width = window.innerWidth;
+        width <= 950
+            ? this.setState({ isMobile: true })
+            : this.setState({ isMobile: false });
+        if (this.state.isHamburgerOpen) {
+            let menu = this.sideMenu.current;
+            width > 550
+                ? menu.style.right = (width - 300) * (-1) + "px"
+                : menu.style.right = 0;
         }
     }
 
     render() {
         if (this.state.loading) return null;
         return (
-            <nav>
+            <nav className="main-nav-bar">
                 <div className="main-nav">
                     <img className="logo" alt="logo" src={logo} 
                          onClick={this.handleLogoClick} />
-                    {this.leftContentSwitcher()}
-                    <NavLink to="/cities" id="cities-link">
-                        cities
-                    </NavLink>
-                    <NavLink to="/about">
-                        about
-                    </NavLink>
-                    <button className="hamburger-button">
-                        <i class="fas fa-bars"></i>
-                    </button>
-                    {this.rightContentSwitcher()}
+                    {this.contentSwitcher()}
                 </div>
             </nav>
         );
